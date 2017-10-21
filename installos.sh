994ac79aca2ccec4da9d18e5e5fcc2e2c63379c0
sini6a <sini6a@live.com>
2017-10-21 17:49:53 +0200

    updated

--- installos.sh
+++ installos.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-echo "This script assumes that you have already prepared HDD for Arch Linux install, and mounted it on /mnt.\n If this isn't the case then please press Ctrl + C or install it by yourself. Thank you."
+echo "This script assumes that you have already prepared HDD for Arch Linux install, and mounted it on /mnt."
+echo "If this isn't the case then please press Ctrl + C or install it by yourself. Thank you."
 for i in {15..0}; do echo -ne "$i"'.. '; sleep 1; done; echo
 clear
 
 echo "Setting TimeDateCTL"
 timedatectl set-ntp true
-sleep 1
+sleep 3
 clear
 
 echo "Copying chroot.sh to /mnt"
-cp ~/toinstall/chroot.sh /mnt
-sleep 1
+cp ./chroot.sh /mnt
+sleep 3
 clear
 
 echo "Pacstrap-ing into /mnt - base & base-devel packages"
-pacstrap /mnt base base-devel
-sleep 1
+pacstrap /mnt base base-devel --needed
+sleep 3
 clear
 
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
-sleep 1
+sleep 3
 clear
 
 echo "Chroot /mnt and exec chroot.sh"
 arch-chroot /mnt ./chroot.sh
-sleep 1
+sleep 3
 clear
 
 
