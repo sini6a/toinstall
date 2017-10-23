7f4dbcc97ceee06e45581eacf5761fc52a4b32df
sini6a <sini6a@live.com>
2017-10-23 22:14:14 +0200

    UPDATE

---
+++ dialog.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dialog --title "Arch Linux Install Script - by Sini6a" --msgbox "This script allows you to install Arch Linux easy way. It was meant only for my config's so please use with caution. " 0 0
+dialog --title "Arch Linux Install Script" --checklist "Choose your option:" 0 0 0 " " "Install FSTAB" 1
+dial
