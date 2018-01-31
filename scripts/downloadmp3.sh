ee138e5350fd5e7d6273d31776959dea607a0e62
sini6a <sini6a@live.com>
2018-01-31 03:06:10 +0100

    Updated colors

--- scripts/downloadmp3.sh
+++ scripts/downloadmp3.sh
@@ -18,14 +18,14 @@ echo -e "--> ${RED}FFMPEG ${NC}and ${RED}YOUTUBE-DL${NC} are required."
 echo -e "--> Press ${BLUE}CTRL+C${NC} anytime to stop." 
 echo -e "--> MP3's are saved at ${GREEN}~/Desktop/MP3s/${NC}"
 echo
-echo -e "--> Session name: ${RED}$session${NC}."
+echo -e "--> Session name: ${BLUE}$session${NC}."
 echo
 
 while true; do
-i=i+1
+i=$[$i+1]
 read -p "-> Enter URL: " URL
-youtube-dl -e $URL
+echo -e "${RED}-> $(youtube-dl -e $URL)${NC}"
 echo "-> Downloading..."
 youtube-dl --quiet -x --audio-format mp3 --audio-quality 320K -o "~/Desktop/MP3s/$session/%(title)s.%(ext)s" $URL
-echo "-> $i song downloaded! "
+echo "-> $i song/s downloaded! "
 done
