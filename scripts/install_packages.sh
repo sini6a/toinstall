9689edcaf16c96471f5ea92536addbded72ac7fc
sini6a <sini6a@live.com>
2018-05-26 14:16:06 +0200

    Updated packages

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -10,10 +10,12 @@ if [ "$os" == 'armv7l' ]; then
 echo "--- Installing packages (armv7l) ---"
 sleep 3
 (yaourt -Sy ~/toinstall/packages.txt --needed && clear) || sleep infinity;
+(yaourt -Sy ~/toinstall/packages_aur.txt --needed && clear) || sleep infinity;
 echo "--> Packages installed! <--" && sleep 3
 else
 echo "--- Installing packages ---"
 sleep 3
 (yaourt -Sy ~/toinstall/packages.txt --noconfirm --needed && clear) || sleep infinity;
+(yaourt -Sy ~/toinstall/packages_aur.txt --noconfirm --needed && clear) || sleep infinity;
 echo "--> Packages installed! <--" && sleep 3
 fi
