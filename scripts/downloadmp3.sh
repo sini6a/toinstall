d105e86cc8e4661d5f68fce9e3451e05b86a96ec
sini6a <sini6a@live.com>
2018-05-27 16:20:24 +0200

    UPDATED SCRIPTS

--- scripts/downloadmp3.sh
+++ scripts/downloadmp3.sh
@@ -4,34 +4,41 @@ GREEN='\e[32m'
 BLUE='\e[34m'
 NC='\e[0m'
 clear
-i=0
 
-read -p '-> Enter session name: ' session
-if [ "$session" == "" ]; then
+i=0
+LOCATION="$HOME/Desktop/MP3s/"
+
+read -p '-> Enter session name: ' SESSION
+if [ "$SESSION" == "" ]; then
 session=$(date '+%d.%m.%Y')
 fi
-mkdir -p ~/Desktop/MP3s/"$session"
+mkdir -p "$LOCATION""$SESSION"
 
 main_prompt() {
 clear
 echo -e "--> Download from web to MP3 320K."
 echo -e "--> ${RED}FFMPEG ${NC}and ${RED}YOUTUBE-DL${NC} are required."
 echo -e "--> Press ${BLUE}CTRL+C${NC} anytime to stop." 
-echo -e "--> MP3's are saved at ${GREEN}~/Desktop/MP3s/$session${NC}"
+echo -e "--> MP3's are saved at ${GREEN}$LOCATION$SESSION${NC}"
 echo
-#echo -e "--> Session name: ${BLUE}$session${NC}."
-echo -e "--> ${GREEN}$i${NC} item(s) downloaded! "
+echo -e "--> ${GREEN}$playlist_items${NC} item(s) in playlist! "
 echo
 }
 
 while true; do
+playlist_items="$(cat $LOCATION$SESSION/$SESSION.txt | wc -l)"
 main_prompt
 read -p '-> Enter URL: ' URL
 echo -e "--> Loading, please wait ..."
 
+if [ "$URL" == "ls" ]; then
+echo -e "${GREEN}$(cat $LOCATION$SESSION/$SESSION.txt)${NC}" | more
+echo
+read -n 1 -s -r -p "-> Press any key to continue"
+else
 youtube-dl -e --quiet --ignore-errors "$URL" >> ~/.mp3tmp
+cat ~/.mp3tmp >> "$LOCATION$SESSION/$SESSION.txt"
 num_of_items="$(cat ~/.mp3tmp | wc -l)"
-i="$(($i+$num_of_items))"
 
 echo
 echo -e "${GREEN}$(cat ~/.mp3tmp)${NC}"
@@ -39,6 +46,7 @@ echo
 
 echo "--> Downloading $(cat ~/.mp3tmp | wc -l) item(s) ..."
 rm ~/.mp3tmp
-youtube-dl --quiet --ignore-errors -x --audio-format mp3 --audio-quality 320K -o "~/Desktop/MP3s/$session/%(title)s.%(ext)s" "$URL"
-sleep 3
+youtube-dl --quiet --ignore-errors -x --audio-format mp3 --audio-quality 320K -o "$LOCATION$SESSION/%(title)s.%(ext)s" "$URL"
+sleep 0.5
+fi
 done
