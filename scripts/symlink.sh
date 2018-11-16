ccd3c3c3e836885a1b0c2ceebf048e149d069a7c
sini6a <sini6a@live.com>
2018-11-16 21:17:44 +0100

    Added Openbox configs

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -7,17 +7,32 @@ echo "--> YAOURT Installed! <--"
 echo "--> Packages installed! <--"
 echo "--- Symlinking config files ---"
 sleep 3
+
 # Create needed directories
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/rofi
+mkdir -p ~/.config/openbox
+
 # Copy rofi config
 cp ~/toinstall/configs/rofi_config ~/.config/rofi/config.rasi
-# Symlink everything else
-ln -sfv ~/toinstall/configs/i3_config ~/.config/i3/config
+
+# Symlink bash and X
 ln -sfv ~/toinstall/configs/bash_config ~/.bashrc
 ln -sfv ~/toinstall/configs/conky_config ~/.conkyrc
-ln -sfv ~/toinstall/configs/i3blocks_config ~/.i3blocks.conf
-ln -sfv ~/toinstall/configs/compton_config ~/.compton.conf
 ln -sfv ~/toinstall/configs/Xresources ~/.Xresources
 ln -sfv ~/toinstall/configs/xinitrc ~/.xinitrc
+
+# Symlink compton
+ln -sfv ~/toinstall/configs/compton_config ~/.compton.conf
+
+# Symlink i3
+ln -sfv ~/toinstall/configs/i3/i3_config ~/.config/i3/config
+ln -sfv ~/toinstall/configs/i3/i3blocks_config ~/.i3blocks.conf
+
+# Symlink openbox
+ln -sfv ~/toinstall/configs/openbox/main_config ~/.config/openbox/rc.xml
+ln -sfv ~/toinstall/configs/openbox/autostart ~/.config/openbox/autostart
+ln -sfv ~/toinstall/configs/openbox/menu_config ~/.config/openbox/menu.xml
+
 echo "--> Everything symlinked! <--" && sleep 3 && clear
+
